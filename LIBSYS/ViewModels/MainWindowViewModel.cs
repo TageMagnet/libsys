17ff28362d22b2a1f5d074c56510fbe7e0f6495f
Harliin <56713048+Harliin@users.noreply.github.com>
2020-04-21 15:14:03 +0200

    updated blabla

--- LIBSYS/ViewModels/MainWindowViewModel.cs
+++ LIBSYS/ViewModels/MainWindowViewModel.cs
@@ -6,6 +6,6 @@ namespace LIBSYS.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        public string Greeting => "Welcome LIBSYS!";
     }
 }
