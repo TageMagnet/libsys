130e312c6a7c99017a79d83be2a7861a6e50beb6
thomascarleberg <thomas_carlsson@hotmail.com>
2020-04-22 12:40:33 +0200

    Lägger till koppling till databas

--- LIBSYS/Models/Repository.cs
+++ LIBSYS/Models/Repository.cs
@@ -6,6 +6,11 @@ namespace LIBSYS.Models
 {
     public class Repository
     {
-
+        public Repository()
+        {
+            ConnectionString = "Data Source=SQL6009.site4now.net;Initial Catalog=DB_A53DDD_Grupp1;User Id=DB_A53DDD_Grupp1_admin;Password=Password123;";
+            connection = new SqlConnection(ConnectionString);
+            connection.Open();
+        }
     }
 }
