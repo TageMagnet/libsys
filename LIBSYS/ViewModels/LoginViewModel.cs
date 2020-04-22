73ef918166526512612e28ddfe952d0adfc78dac
thomascarleberg <thomas_carlsson@hotmail.com>
2020-04-22 11:13:28 +0200

    Update LoginViewModel.cs

--- LIBSYS/ViewModels/LoginViewModel.cs
+++ LIBSYS/ViewModels/LoginViewModel.cs
@@ -4,9 +4,12 @@ using System.Text;
 
 namespace LIBSYS.ViewModels
 {
+
+    //
     public class LoginViewModel
     {
-        
+        public eStatus Status { get; set; }
+        public enum eStatus { Admin = 1, Librarian = 2, Customer = 3, Guest = 4 }
         public int Role { get; set; }
 
     }
