9bfa657cec94b99568d28dae1e88dc5b7b45f051
thomascarleberg <thomas_carlsson@hotmail.com>
2020-04-22 11:55:48 +0200

    Ändringar

--- LIBSYS/Models/User.cs
+++ LIBSYS/Models/User.cs
@@ -6,6 +6,8 @@ namespace LIBSYS.Models
 {
     class User
     {
+        public eStatus Status { get; set; }
+        public enum eStatus { Admin = 1, Librarian = 2, Customer = 3, Guest = 4 }
         public string Email { get; set; }
         public string NickName { get; set; }
         public string Password { get; set; }
