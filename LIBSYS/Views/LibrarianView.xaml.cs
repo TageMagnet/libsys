45526a4e0419cb2fd05d9f912bd91328013bd22d
Pontuspe <pontus-pettersson@hotmail.com>
2020-04-22 12:00:41 +0200

    La till views / viewmodels för alla

---
+++ LIBSYS/Views/LibrarianView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LIBSYS.Views
+{
+    public class LibrarianView : UserControl
+    {
+        public LibrarianView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
