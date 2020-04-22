208a34194211e70f194155ac10d479af8a604b65
thomascarleberg <thomas_carlsson@hotmail.com>
2020-04-22 11:04:56 +0200

    Create LoginViewModel.cs

---
+++ LIBSYS/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LIBSYS.ViewModels
+{
+    public class LoginViewModel
+    {
+        
+        public int Role { get; set; }
+
+    }
+}
