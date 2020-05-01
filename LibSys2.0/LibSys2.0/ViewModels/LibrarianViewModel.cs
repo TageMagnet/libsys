26b334fb6bb2b249b45c3117685c8c2ba6c75b2a
Pontuspe <pontus-pettersson@hotmail.com>
2020-05-01 16:05:36 +0200

    Update LibrarianViewModel.cs

--- LibSys2.0/LibSys2.0/ViewModels/LibrarianViewModel.cs
+++ LibSys2.0/LibSys2.0/ViewModels/LibrarianViewModel.cs
@@ -24,21 +24,14 @@ namespace LibrarySystem.ViewModels
         public ReactiveCommand<Unit,Unit> AddBook { get; set; }
         public LibrarianViewModel()
         {
-<<<<<<< HEAD
-            if (Books == null)
-            {
-                Books = new List<Book>();
-            }
             LoadDataAsync();
-=======
-            
-            
->>>>>>> af041bc2d08f787e1503e7d6d95cb5de95678236
         }
 
         public async void LoadDataAsync()
         {
-            await Task.Run(() => LoadBooks());
+            await LoadBooks();
+            await LoadEbooks();
+            await LoadEvents();
         }
 
         public async Task LoadBooks()
