9098d47da8a3ee9469f3601129545ca0c070502a
Pontuspe <pontus-pettersson@hotmail.com>
2020-05-01 15:48:30 +0200

    update

--- LibSys2.0/LibSys2.0/ViewModels/LibrarianViewModel.cs
+++ LibSys2.0/LibSys2.0/ViewModels/LibrarianViewModel.cs
@@ -1,38 +1,53 @@
-﻿using LibrarySystem.Models;
+﻿using Library;
+using LibrarySystem.Models;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.Reactive;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace LibrarySystem.ViewModels
 {
     public class LibrarianViewModel
     {
-        //public Repository repo = new Repository();
-        //public string Title { get; set; }
-        //public double Completion { get; set; }
+        public BookRepository bookRepo = new BookRepository();
+        public eBookRepository eBookRepo = new eBookRepository();
+        public EventRepository eventRepo = new EventRepository();
+        public string Title { get; set; }
+        public double Completion { get; set; }
 
         public List<Event> ListOfEvents { get; set; }
         public List<Book> Books { get; set; }
+        public List<eBook> eBooks { get; set; }
+        public List<Event> Event { get; set; }
+        public ReactiveCommand<Unit,Unit> AddBook { get; set; }
         public LibrarianViewModel()
         {
             if (Books == null)
             {
                 Books = new List<Book>();
             }
-            Books.Add(new Book { title = "Harry Potter", description = "HARRY BECOMES A WIZZARD YO!" });
-            Books.Add(new Book { title = "Harry Potter 2", description = "HARRY IS NOW A WIZZARD" });
-            Books.Add(new Book { title = "Harry Potter 3", description = "HARRY LOST HIS WAND" });
-            Books.Add(new Book { title = "Harry Potter 3", description = "HARRY LOST HIS WAND" });
-            Books.Add(new Book { title = "Harry Potter 3", description = "HARRY LOST HIS WAND" });
-            Books.Add(new Book { title = "Harry Potter 3", description = "HARRY LOST HIS WAND" });
-            Books.Add(new Book { title = "Harry Potter 3", description = "HARRY LOST HIS WAND" });
-            Books.Add(new Book { title = "Harry Potter 3", description = "HARRY LOST HIS WAND" });
-            Books.Add(new Book { title = "Harry Potter 3", description = "HARRY LOST HIS WAND" });
-            Books.Add(new Book { title = "Harry Potter 3", description = "HARRY LOST HIS WAND" });
-            Books.Add(new Book { title = "Harry Potter 3", description = "HARRY LOST HIS WAND" });
-            Books.Add(new Book { title = "Harry Potter 3", description = "HARRY LOST HIS WAND" });
+            
         }
 
+        public async void LoadDataAsync()
+        {
+
+        }
+
+        public async Task LoadBooks()
+        {
+            Books = new List<Book>(await bookRepo.ReadAll());
+        }
+        public async Task LoadEbooks()
+        {
+            eBooks = new List<eBook>(await eBookRepo.ReadAll());
+        }
+        public async Task LoadEvents()
+        {
+            Event = new List<Event>(await eventRepo.ReadAll());
+        }
         //private ObservableCollection<Event> events;
         //public ObservableCollection<Event> Events
         //{
