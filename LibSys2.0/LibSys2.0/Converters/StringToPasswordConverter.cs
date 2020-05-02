3ff312868d06209724c6b1b5084567f71c5e6a51
jakob andersson <jakob.aah@gmail.com>
2020-05-02 23:19:23 +0200

    Password Masking '*' for Textbox 	- new folder '/Converters' 	- new class StringToMaskedConverter 		Converts and deconverts inputstring

---
+++ LibSys2.0/LibSys2.0/Converters/StringToPasswordConverter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace LibrarySystem.Converters
+{
+    /// <summary>
+    /// Konvertar en string till maskade karaktärer
+    /// </summary>
+    public class StringToPasswordConverter : IValueConverter
+    {
+        /// <summary>
+        /// Sparar och håller reda på den 'rena' stringen för konvertering tillbaka
+        /// </summary>
+        private static string StoredCleanPassword;
+
+        /// <summary>
+        /// String till maskad
+        /// </summary>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            string cleanPassword = (string)value;
+            string maskedPassword = "";
+
+            foreach(char chr in cleanPassword)
+            {
+                maskedPassword += '*';
+            }
+
+            StoredCleanPassword = cleanPassword;
+
+            return maskedPassword;
+        }
+
+        /// <summary>
+        /// Maskad till ren string
+        /// </summary>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            string maskedPassword = (string)value;
+            string cleanPassword = "";
+            // Indexer for the stored password
+            int j = 0;
+
+            foreach (char chr in maskedPassword)
+            {
+                //todo; check if password containing an '*' works
+                if (chr == '*')
+                    cleanPassword += StoredCleanPassword[j];
+                else
+                    cleanPassword += chr;
+
+                j++;
+            }
+            return cleanPassword;
+        }
+    }
+}
