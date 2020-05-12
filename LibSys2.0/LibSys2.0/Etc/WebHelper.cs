b2fad27c0ae17bc21bd618ea4d43a3ed7d92da88
jakob andersson <jakob.aah@gmail.com>
2020-05-12 18:24:45 +0200

    pulling master, squash this

---
+++ LibSys2.0/LibSys2.0/Etc/WebHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net;
+
+namespace LibrarySystem.Etc
+{
+    /// <summary>
+    /// Web requests etc. Gör saker över internet
+    /// </summary>
+    public static class WebHelper
+    {
+        public static async void UploadCoverImage()
+        {
+            string url = "http://api.jkb.zone/upload";
+            string method = "POST";
+
+
+            WebRequest request = WebRequest.Create(url);
+            request.Method = method;
+
+        }
+    }
+}
