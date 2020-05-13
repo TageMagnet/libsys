0835483ecf231908bb8fe674e85e3327ed278575
jakob andersson <jakob.aah@gmail.com>
2020-05-13 21:59:31 +0200

    Button/route swap around && renaming

--- LibSys2.0/LibSys2.0/ViewModels/HeaderViewModel.cs
+++ LibSys2.0/LibSys2.0/ViewModels/HeaderViewModel.cs
@@ -8,14 +8,13 @@ namespace LibrarySystem.ViewModels
 {
     public class HeaderViewModel : BaseViewModel
     {
-        public ReactiveCommand<string, Unit> GoHomeCommand { get; set; } = ReactiveCommand.Create((string vmName) => MainWindowViewModel.ChangeView(vmName));
-        public ReactiveCommand<Unit, Unit> LoginCommand { get; set; } = ReactiveCommand.Create(() => MainWindowViewModel.ChangeView("librarian"));
-        public ReactiveCommand<Unit, Unit> RegisterCommand { get; set; } = ReactiveCommand.Create(() => MainWindowViewModel.ChangeView("register"));
+        public ReactiveCommand<string, Unit> GoHome { get; set; } = ReactiveCommand.Create((string vmName) => MainWindowViewModel.ChangeView(vmName));
+        public ReactiveCommand<Unit, Unit> GoToLogin { get; set; } = ReactiveCommand.Create(() => MainWindowViewModel.ChangeView("login"));
+        public ReactiveCommand<Unit, Unit> GoToRegister { get; set; } = ReactiveCommand.Create(() => MainWindowViewModel.ChangeView("register"));
+        public ReactiveCommand<Unit, Unit> ByPassIntoBackend { get; set; } = ReactiveCommand.Create(() => MainWindowViewModel.ChangeView("librarian"));
 
         public bool IsLoggedIn { get => !string.IsNullOrEmpty(MainWindowViewModel.CurrentLoggedInMember.role); }
         /// <summary> Extends the currently logged in member from MainWindow </summary>
         public Models.Member CurrentLoggedInMember { get => MainWindowViewModel.CurrentLoggedInMember; set => MainWindowViewModel.CurrentLoggedInMember = value; }
-
-
     }
 }
