b5070ca5a30b76d955477546411a21f8b31dca90
thomascarleberg <thomas_carlsson@hotmail.com>
2020-05-13 14:07:49 +0200

    Update MemberRepository.cs

--- LibSys2.0/LibSys2.0/Library/Repository/MemberRepository.cs
+++ LibSys2.0/LibSys2.0/Library/Repository/MemberRepository.cs
@@ -24,7 +24,7 @@ namespace Library
             {
                 try
                 {
-                    List<Member> member = (await connection.QueryAsync<Member>($"SELECT member_id, email, nickname, pwd, role_name as 'role', created_at, is_active, ref_member_role_id FROM members M INNER JOIN member_roles MR ON M.ref_member_role_id = MR.member_role_id WHERE M.is_active = 1")).ToList();
+                    List<Member> member = (await connection.QueryAsync<Member>($"SELECT member_id, email, nickname, pwd, role_name as 'role', created_at, is_active, ref_member_role_id FROM members M INNER JOIN Member_roles MR ON M.ref_member_role_id = MR.member_role_id WHERE M.is_active = 1")).ToList();
                     return member;
                 }
                 catch (Exception e)
