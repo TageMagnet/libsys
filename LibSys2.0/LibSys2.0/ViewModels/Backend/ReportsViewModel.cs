e404ea4b3fed8f2396fe626d845d27c3e2c2e401
Harliin <56713048+Harliin@users.noreply.github.com>
2020-05-26 14:03:24 +0200

    comiit

---
+++ LibSys2.0/LibSys2.0/ViewModels/Backend/ReportsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using LibrarySystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibrarySystem.ViewModels.Backend
+{
+    public class ReportsViewModel: BaseViewModel
+    {
+
+        public ReportsViewModel()
+        {
+
+        }
+        public ReportsViewModel(Member member)
+        {
+
+        }
+    }
+}
